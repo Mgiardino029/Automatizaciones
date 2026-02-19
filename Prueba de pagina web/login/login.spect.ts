d2572bcbcd0186fe5878a00c6e5def7804029792
Mgiardino029 <giardinomatias029@gmail.com>
2026-02-19 20:24:45 -0300

    update

--- Prueba de pagina web/login/login.spect.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('login exitoso', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.fill('#user-name', 'standard_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
-  await expect(page).toHaveURL('https://www.saucedemo.com/');
-});
